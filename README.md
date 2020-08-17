1a16a830bf7537c449d6fd4dd7e948ac0da080b3
Away-Wolf <3245632373@qq.com>
2020-08-17 17:24:12 +0800

    自述文件

---
+++ README.md
@@ -0,0 +1,20 @@
+# MySpace
+
+## 项目介绍：
+
+- 由孤痕独立开发的命令行工具
+
+- 拥有多种常用使用功能
+
+- 使用Node.js开发以及commander.js等模块
+
+## 项目功能
+
+- 创建不同的类型的项目
+- 时间操作
+- 计划操作
+
+## 文件分布
+
+<img src="http://pic.yupoo.com/awat/9af64a23/63f2cb52.png" alt="20">
+
--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,45 @@
 #! /usr/bin/env node
 const program = require('commander');
+const {Command} = require('commander');
+const chalk = require('chalk');
+
 
 program // 创建模板文件
-    .command('create <projectName>')
+    .command('create <projectName> [otherNames...]')
     .option('-f, --frontweb', 'create front-web files')
     .option('-C, --cpplus', 'create c plus plus files')
     .option('-p --python', 'create python files')
     .option('-c --cfiles', 'create c files')
-    .action(function (projectName, cmd) {
-        var options = [cmd.frontweb, cmd.cpplus, cmd.python, cmd.cfiles];
-        var options_n = ['frontweb', 'cpplus', 'python', 'cfiles'];
-        var temp = [];
-        var file_n;
+    .action(function (projectName, otherNames, cmd) {
+        names = []
+        //console.log(projectName);
+        names.push(projectName)
+        if (otherNames) {
+            names = names.concat(otherNames);
+        }
+        //console.log(names)
+        //console.log(cmd)
+        var options = [cmd.frontweb ? true : false, cmd.cpplus ? true : false, cmd.python ? true : false, cmd.cfiles ? true : false];
+        var options_name = ['frontweb', 'cpplus', 'python', 'cfiles'];
+        var temp = []
         for (var i = 0;i < options.length;i++) {
             if (options[i] === true) {
                 temp.push('true');
             }
         }
         if (temp.length > 1) {
-            console.log('Error:Please Write an option');
+            console.log(chalk.red('Error:') + chalk.blue('Please Write an option'));
         }
         else {
-            file_n = options_n[options.indexOf(true)];
-            require('../lib/command/create.js')(projectName, file_n);
-        
+            file_name = options_name[options.indexOf(true)];
+            require('../lib/command/create.js')(names, file_name);
         }
         
-    });
+    })
 
-program // 查看my当前版本
-    .version('0.0.1', '-v, --version')
-    .option('-s --settings', 'settings')
+/*program // 查看my当前版本*/
+    //.version('0.0.1', '-v, --version')
+    /*.option('-s --settings', 'settings')*/
 
 	
 
